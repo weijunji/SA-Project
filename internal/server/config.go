c89ac949406ca54c56539e311bc81d8a66137663
weijunji <1049918689@qq.com>
2020-10-15 08:16:46 +0800

    server config

---
+++ internal/server/config.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"io/ioutil"
+
+	log "github.com/sirupsen/logrus"
+	. "github.com/weijunji/SA-Project/pkg/tools"
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	port int32
+	auth string
+}
+
+const configFileName string = "sap_server.yaml"
+
+var conf Config
+
+func init() {
+	log.Info("Read config file")
+	if !FileExist(configFileName) {
+		log.Fatal("Cannot find config file")
+	} else {
+		readConfig()
+	}
+}
+
+func readConfig() {
+	buf, err := ioutil.ReadFile(configFileName)
+	HandleErrPanic(err)
+	err = yaml.Unmarshal(buf, &conf)
+	HandleErrPanic(err)
+}
